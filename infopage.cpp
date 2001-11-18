3aff36257b8c2c599288ebdccc29e90d654ad510
Jesper Pedersen <blackie@blackie.dk>
2001-11-18 15:07:32 +0000

    work arround for Qt3 source incompatible change
    svn path=/trunk/kdeutils/kregexpeditor/; revision=123036

--- infopage.cpp
+++ infopage.cpp
@@ -2,6 +2,7 @@
 #include <klocale.h>
 #include <kurl.h>
 #include <kapplication.h>
+#include <kdebug.h>
 
 InfoPage::InfoPage( QWidget* parent, const char* name )
   :QTextBrowser( parent, name )
@@ -9,34 +10,41 @@ InfoPage::InfoPage( QWidget* parent, const char* name )
   setText( i18n( "<qt>"
                  "<h1>Regular Expression Editor</h1>"
                  "What you are currently looking at is an editor for <i>Regular Expressions</i>.<p>"
-                 "You start editing by pressing one of the buttons at the left, and then fill out "
-                 "the area containing the text you are looking at, by pressing the left mouse button at the location "
-                 "you want the items to apear.<p>"
+                 "The window to the right is the editing area. The row of buttons is the editing actions. "
+                 "This is actually very similar to common drawing programs. Select an editing tool to start "
+                 "editing your regular expression.<p>"
 
                  "<h2>What is a regular expression?</h2>"
                  "If you do not know what a regular expression is, then it might be a good idea "
-                 "to read <a href=\"doc:whatIsARegExp\">the introduction</a>.<p>"
+                 "to read <a href=\"doc://whatIsARegExp\">the introduction</a>.<p>"
 
                  "<h2>How does this editor work?</h2>"
                  "In case this is the first time you see this editor, then you might want to " 
-                 "read an <a href=\"doc:theEditor\">introduction to the editor</a>.<p><p>"
+                 "read an <a href=\"doc://theEditor\">introduction to the editor</a>.<p><p>"
                  "The following list might help you find exactly what you are searching for in the documentation:"
                  "<ul>"
-                 "<li> <a href=\"doc:widgets\">Description of the Widgets</a>."
-                 "<li> <a href=\"doc:cutnpaste\">Cut and Past</a> and <a href=\"doc:draganddrop\">Drag and Drop</a>."
-                 "<li> <a href=\"doc:loadandsave\">Load and Save</a>"
-                 "<li> <a href=\"doc:graphicalvsascii\">The Graphical Widget v.s. the Ascii Syntax.</a>"
+                 "<li> <a href=\"doc://widgets\">Description of the editing tools</a>."
+                 "<li> <a href=\"doc://cutnpaste\">Cut and Past</a> and <a href=\"doc://draganddrop\">Drag and Drop</a>."
+                 "<li> <a href=\"doc://loadandsave\">Load and Save</a>"
+                 "<li> <a href=\"doc://graphicalvsascii\">The Graphical Editor v.s. the Ascii Editor.</a>"
+                 "<li> <a href=\"doc://bugreport\">Reporting bugs or suggesting new features</a>"
                  "</ul>"
 
-                 "<h2>Authors</h2>"
+                 "<h2>Send the author an electronic postcard</h2>"
+                 "As everybody else in the KDE project I dont get any money for my work on KDE. "
+                 "I therefore appreciate it very much when users tell me what they think about my work. "
+                 "I would therefor be very happy if you <a href=\"mailto:blackie@kde.org?subject=KRegExpEditor\">"
+                 "sent me a short mail</a>, telling me that you use my regular expression editor."
+
+                 "<h2>Author</h2>"
                  "<a href=\"http://www.blackie.dk/\">Jesper K. Pedersen</a> &lt;<a href=\"mailto:blackie@kde.org\">blackie@kde.org</a>&gt;"
                  "</qt>") );
 }
 
 void InfoPage::setSource ( const QString & name )
 {
-  if ( name.left(4) == QString::fromLocal8Bit("doc:") ) {
-    kapp->invokeHelp( name.mid(4), QString::fromLocal8Bit("KRegExpEditor") ); 
+  if ( name.left(6) == QString::fromLocal8Bit("doc://") ) {
+    kapp->invokeHelp( name.mid(6, name.length()-7), QString::fromLocal8Bit("KRegExpEditor") ); 
   }
   else if ( name.left(7) == QString::fromLocal8Bit("mailto:") ) {
     kapp->invokeMailer( KURL(name) );
