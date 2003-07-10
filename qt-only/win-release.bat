430b4995f40c2e38268272610302f37428f0872e
Jesper Pedersen <blackie@blackie.dk>
2003-07-10 15:38:59 +0000

    minor changes
    svn path=/trunk/kdeutils/kregexpeditor/; revision=236417

--- qt-only/win-release.bat
+++ qt-only/win-release.bat
@@ -1,6 +1,6 @@
 md kregexpeditor
 cp -r kregexpeditor.exe predefined kregexpeditor
-copy q:\3.1.2-mt\lib\qt-mt312.* kregexpeditor
+copy q:\3.1.2-mt\lib\qt-mt312.dll kregexpeditor
 copy c:\winnt\system32\MSVCRTD.DLL kregexpeditor
 copy c:\winnt\system32\MSVCP60D.DLL kregexpeditor
 cp -r manual kregexpeditor
