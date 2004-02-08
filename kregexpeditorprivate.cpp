f878132ac9b2c8961a9b77ca60ee4ed3edca2aee
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2004-02-08 00:48:41 +0000

    evelop -> develop. Thanks Jesper
    svn path=/trunk/kdeutils/kregexpeditor/; revision=285985

--- kregexpeditorprivate.cpp
+++ kregexpeditorprivate.cpp
@@ -166,7 +166,7 @@ KRegExpEditorPrivate::KRegExpEditorPrivate(QWidget *parent, const char *name)
   QWhatsThis::add( _regexpEdit, i18n( "This is the regular expression in ASCII syntax. You are likely only "
 				      "to be interested in this if you are a programmer, and need to "
 				      "develop a regular expression using QRegExp.<p>"
-                                      "You may evelop your regular expression both by using the graphical "
+                                      "You may develop your regular expression both by using the graphical "
 				      "editor, and by typing the regular expression in this line edit.") );
 
 #ifdef QT_ONLY
