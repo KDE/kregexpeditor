db41a9ee2a05fb44bfd10c228ba08182e7f116af
Morten Sjøgren <m_abs@mabs.dk>
2011-10-18 20:11:08 +0200

    Update TODO-list and make README into a script

--- make_qrexexparser.sh
+++ make_qrexexparser.sh
@@ -1,3 +1,4 @@
-Bison and Flex command lines :
+#!/bin/bash
+# Bison and Flex command lines :
 flex -Pqregexp -ogen_qregexplexer.cpp qregexpparser.l
 bison -d -p qregexp -o gen_qregexpparser.cc qregexpparser.y
