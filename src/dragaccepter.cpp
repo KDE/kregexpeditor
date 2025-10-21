85b98d837c9f23d81d3cf9ec920a8cd3480c6164
Matthias Mailänder <matthias@mailaender.name>
2025-10-21 13:24:50 +0200

    Fix e-Mail address.

--- src/dragaccepter.cpp
+++ src/dragaccepter.cpp
@@ -1,5 +1,5 @@
 /*
- *  SPDX-FileCopyrightText: 2002-2003 Jesper K. Pedersen <Qt::blackie@kde.org>
+ *  SPDX-FileCopyrightText: 2002-2003 Jesper K. Pedersen <blackie@kde.org>
  *
  *  SPDX-License-Identifier: LGPL-2.0-only
  **/
